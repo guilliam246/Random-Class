1dd4fa37c5682da259d6400c4475d984b6d4b064
Guilliam Oberholster <guilliamoberholster@gmail.com>
2022-05-24 13:59:52 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Random_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(random.Next(1,10));
+
+        }
+    }
+}
